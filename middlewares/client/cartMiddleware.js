c8f956fc158f2323d2b80741fc3914bb7321b00c
Lngngyn289 <longnguyenduc98921@gmail.com>
2024-12-14 19:42:25 +0700

    fix deploy

--- middlewares/client/cartMiddleware.js
+++ middlewares/client/cartMiddleware.js
@@ -9,10 +9,12 @@ module.exports.cartId = async (req, res, next) => {
     res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresCookie)
     })
+    console.log(cart)
   } else{
     const cart = await Cart.findOne({
       _id: req.cookies.cartId
     })
+    console.log(cart);
     cart.totalQuantity = cart.products.reduce((sum,item) => sum + item.quantity,0)
     res.locals.miniCart = cart
   }
